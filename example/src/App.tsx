81c76548c908750274da136010e98a62631661c0
Taranjeet Singh <taranjeet.singh@telekom-digital.com>
2022-01-14 12:52:21 +0530

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-import { Button } from '@minimal_ui/theme';
+import { Button, Select, ListItem } from '@minimal_ui/theme';
 
 const App = () => {
-  return <Button>
+  return <div>
+    <Button>
     Hello
   </Button>
+
+
+  <br />
+
+  <Select>
+    <ListItem value={4} label={"Four"}>FOur</ListItem>
+    <ListItem value={5} label={"Five"}>Five</ListItem>
+    <ListItem value={6} label={"Six"}>Six</ListItem>
+  </Select>
+  </div>
 }
 
 export default App
